7ed573a2077dbf1687238f9f7dc8afc827fcfbfe
Bibekananda Mahata <78229916+mahatabibek@users.noreply.github.com>
2024-09-06 12:56:19 +0530

    added basic design-lrn1-6.9.24

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,31 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(const MaterialApp(home: Scaffold(body: Text("Hello"))));
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: SweepGradient(
+              colors: [
+                Color.fromRGBO(13, 214, 67, 0.443),
+                Color.fromARGB(106, 230, 20, 20),
+                Color.fromARGB(136, 16, 6, 212),
+              ],
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              "WTF!",
+              style: TextStyle(
+                color: Color.fromARGB(255, 255, 255, 255),
+                backgroundColor: Color.fromARGB(255, 0, 0, 0),
+                fontSize: 55,
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }
