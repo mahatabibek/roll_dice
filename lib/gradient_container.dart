7a4f5181b8d90786b5924893bf7bfb73f40cd3eb
Bibekananda Mahata <78229916+mahatabibek@users.noreply.github.com>
2024-09-15 15:33:53 +0530

    created rollerclass file & states

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:roll_dice/dice_roller.dart';
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key, required this.colors});
 
   final List<Color> colors;
-  void rollDice() {}
 
   @override
   Widget build(context) {
@@ -14,28 +14,8 @@ class GradientContainer extends StatelessWidget {
           colors: colors,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              'assets/images/dice-1.png',
-              width: 200,
-            ),
-            const SizedBox(height: 20),
-            TextButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                //padding: const EdgeInsets.only(
-                //  top: 20,
-                //),
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(fontSize: 28),
-              ),
-              child: const Text("Roll Dice"),
-            ),
-          ],
-        ),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
